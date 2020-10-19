8ee62e40efc967249fbf7d26a7c3e885b6892257
LuisYairAquino <luisyairam.it@gmail.com>
2020-10-18 22:51:18 -0500

    Tarea para 20 de Octubre

--- Testcafe/practica3.js
+++ Testcafe/practica3.js
@@ -17,7 +17,7 @@ test("Probar que podemos escribir un email", async (t) => {
     .click(page.link3)
 
     await t
-    .typeText(page.input31, 'noemila@noemail.com')
+    .typeText(page.input31, 'noemail@noemail.com')
     .expect(page.input31.value).eql('noemail@noemail.com')
 });
 
@@ -33,5 +33,18 @@ test("Probar que se envía el email correctamente", async (t) => {
 
     await t
     .expect(page.text33.innerText).contains('sent')
+
+});
+
+test("Probar que manda un Internal Server Error", async (t) => {
+    await t
+    .click(page.link3)
+
+    await t
+    .click(page.button32)
+
+    await t
+    .expect(page.internalError.exists).ok()
+    .expect(page.internalError.innerText).contains('Error')
 
 });
