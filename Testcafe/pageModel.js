13718383035f4a3744e9e46671646ad39d168538
LuisYairAquino <luisyairam.it@gmail.com>
2020-10-20 21:17:57 -0500

    Prácticas 5 y 6 de 20 de Octubre

--- Testcafe/pageModel.js
+++ Testcafe/pageModel.js
@@ -25,7 +25,20 @@ class Page {
         this.checkbox42 = Selector('#checkboxes > input[type=checkbox]:nth-child(3)');
 
         //Homework
-        this.internalError = Selector('h1').withText('Internal Server Error')
+        this.internalError = Selector('h1').withText('Internal Server Error');
+        const abText = Selector('h3');
+
+        //Práctica 5
+
+        this.link5 = Selector('a').withText('Inputs');
+        this.input51 = Selector('#content > div > div > div > input[type=number]');
+
+        //Práctica 6
+        this.link6 = Selector('a').withText('Add/Remove Elements');
+        this.addBtn61 = Selector('#content > div > button');
+        this.removeBtn62 = Selector('#elements > button');
+        this.removeBtn63 = Selector('#elements > button:nth-child(2)');
+        this.removeBtn64 = Selector('#elements > button:nth-child(3)');
     
     }
 }
---
+++ Testcafe/practica5.js
@@ -0,0 +1,22 @@
+import page from  './pageModel'
+
+fixture("Práctica 5")
+    .page("https://the-internet.herokuapp.com/");
+
+test('Probar escribir números en un input', async t =>{
+    await t
+        .click(page.link5)
+
+    await t
+        .maximizeWindow()
+        .typeText(page.input51, "letras", {speed: 0.1})
+        .wait(2000)
+        .expect(page.input51.value).eql('')
+
+        .typeText(page.input51, "123456789", {speed: 0.1})
+        .expect(page.input51.value).notEql('222')
+        .expect(page.input51.value).eql("123456789")
+
+
+    
+});
